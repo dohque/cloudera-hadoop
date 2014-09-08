fc6ebf7920f64f2536c308477c660de3371682ba
Ruslan Pilin <dohque@gmail.com>
2014-09-08 19:24:07 +0300

    name in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
-maintainer        "Ruslan Pilin"
-description       "Installs Cloudera Hadoop"
+name              'cloudera-hadoop'
+maintainer        'Ruslan Pilin'
+description       'Installs Cloudera Hadoop'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
-license           "MIT"
+version           '0.0.1'
+license           'MIT'
 
-recipe "cloudera", "Installs Cloudera Pseudo (all on one node)"
+recipe 'cloudera-hadoop', 'Installs Cloudera Pseudo (all on one node)'
 
 %w{ centos }.each do |os|
   supports os
